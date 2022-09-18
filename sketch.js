ee606b74c74f3551043f9352f88ed8b6f64bbde5
lulock <leilamethnani@gmail.com>
2022-09-18 14:20:06 +0200

    made classes for objects Player Tower and Ability Bar UI extending Unit class

--- sketch.js
+++ sketch.js
@@ -3,43 +3,44 @@ let bullets = [],
   player,
   player_2,
   players,
+  abilities_bar,
   particles = [],
-  BULLET_SIZE = 12,
+  BULLET_SIZE = 6,
+  BAR_SIZE = 55,
   IDs = [1,2,3,4,5,6,7,8,9,10],
   timer = 5;
 
   function setup() {
+
+    // hue in 360 degree wheel, saturation, brightness
+    colorMode(HSB);
+
     // set up canvas
     createCanvas(710, 400);
 
+    // init abilities bar ui_unit
+    abilities_bar = new AbilityBar(createVector(width/2 - 50, height - 51), 50)
+
     // init tower unit; spawn randomly
-    tower = new Unit(createVector(random(width-100),random(height-100)), 55);
+    tower = new Tower(createVector(random(width-100),random(height-100)), 55);
 
     // init player unit; spawn in centre of canvas
-    player = new Unit(createVector(width/2,height/2), 20);
+    player = new Player(createVector(width/2,height/2), 20);
     
     // init player 2 unit; spawn in corney of canvas
-    player_2 = new Unit(createVector(100,100), 20);
+    player_2 = new Player(createVector(100,100), 20);
 
-    // add 10 random particles as enemies!
+    // add 10 random particles as enemies! TODO: make particle system?
     for(let i = 0;i<10;i++){
       particles.push(new EnemyParticle());
     }
 
-    // default draw settings
-    strokeWeight(6.0);
-    stroke(50, 50, 20);
-    fill(100)
-
-    // shuffle IDs
+    // shuffle IDs for no good reason
     IDs.sort(() => Math.random() - 0.5)
 }
 
 // every loop, draw to canvas...
 function draw() {
-  
-  // hue in 360 degree wheel, saturation, brightness
-  colorMode(HSB);
   
   // clear background (black)
   background(0);
@@ -49,7 +50,7 @@ function draw() {
     particles[i].createParticle();
     particles[i].moveParticle();
 
-    // check collision with player
+    // check collision with player(s)
     let partCol = Collision(particles[i].x, particles[i].y,particles[i].r,particles[i].r, player.position.x, player.position.y, player.size, player.size);
     let partCol2 = Collision(particles[i].x, particles[i].y,particles[i].r,particles[i].r, player_2.position.x, player_2.position.y, player_2.size, player_2.size);
     
@@ -57,164 +58,59 @@ function draw() {
     if(partCol){
       player.health -=.1*(particles[i].r);
     }
-
     // particle size determines damage to player
     if(partCol2){
       player_2.health -=.1*(particles[i].r);
     }
+
+  }
+
+  // Ctrl
+  if (keyIsDown(17)) {
+    drawLine();
   }
   
   // tower
-  if (tower.health > 0){
-    // draw rectangle with rounded corners dependant on tower health :)
-    
-    // push() function saves current drawing style settings and transformations
-    // pop() restores these settings
-    push()
-    stroke(125+tower.health, 100+tower.health, 50);
-    fill(100);
-    rect(tower.position.x, tower.position.y, tower.size, tower.size, 20, 15+(100/tower.health), 10, 5+(1000/tower.health));
-    describe('white rect with outline and round edges of different radii that changes upon declining health');
-    
-    // tower health bar
-    stroke(50, 50, 20);
-    rect(tower.position.x, tower.position.y-20, tower.size, 5,1)
-    pop()
-    
-    push()
-    stroke(120, 60, 80);
-    rect(tower.position.x, tower.position.y-20, (tower.size * tower.health/100), 5,1)
-    pop()
-  }
+  tower.display()
+  
+  // player 1 bullets 
+  bullets.forEach(function shoot(bullet, index){
+    bullet.createParticle();
+    bullet.moveParticle();
 
-  // player 1
-  if (player.health > 0){
-    push()
-
-    strokeWeight(4);
-    stroke(255, 204, 50);
-    
-    let vec = createVector(mouseX - player.position.x, mouseY - player.position.y)
-    
-    if(vec.mag()>=100){
-      // let normx = vec.x / vec.mag();
-      // let normy = vec.y / vec.mag();
-      // line(player.position.x, player.position.y, player.position.x + 100*normx, player.position.y + 100*normy);
-      line(player.position.x, player.position.y, mouseX, mouseY); // TEMP
-    }else{
-      line(player.position.x, player.position.y, mouseX, mouseY);
+    if (bullet.isDead()){
+      bullets.splice(index, 1);
     }
-    describe('a line running from player to mouse pos');
-
-    pop()
-
-    push()
-    stroke(255, 204, 50);
-    fill(100);
-    strokeWeight(0.0);
-    circle(player.position.x, player.position.y, player.size);
-    describe('white circle with black outline in mid of gray canvas');
-    pop()
-
-    push()
-    // player health bar
-    stroke(50, 50, 20);
-    rect(player.position.x-tower.size/2, player.position.y-30, tower.size, 5,1)
-    pop()
-
-    push()
-    stroke(120, 60, 80);
-    rect(player.position.x-tower.size/2, player.position.y-30, (tower.size * player.health/100), 5,1)
-    pop()
-
-  } 
-
-  // player 2
-  if (player_2.health > 0){
-    push()
-
-    strokeWeight(4);
-    stroke(255, 204, 50);
-    fill(250);
-    strokeWeight(0.0);
-    circle(player_2.position.x, player_2.position.y, player_2.size);
-    describe('white circle with black outline in mid of gray canvas');
-    pop()
-
-    push()
-    // player health bar
-    stroke(50, 50, 20);
-    rect(player_2.position.x-tower.size/2, player_2.position.y-30, tower.size, 5,1)
-    pop()
-
-    push()
-    stroke(120, 60, 80);
-    rect(player_2.position.x-tower.size/2, player_2.position.y-30, (tower.size * player_2.health/100), 5,1)
-    pop()
-
-  } 
-
-  bullets.forEach(function shoot(bvalue, index){
-    push()
-
-    // set colour
-    strokeWeight(2);
-    stroke(255, 204, 150);
-    fill(50, bvalue.lifespan);
-
-    // move bullet
-    bvalue.position.x += bvalue.direction.x * 1/10
-    bvalue.position.y += bvalue.direction.y * 1/10
 
     let col = false;
 
     if (tower.health > 0){
-  
-      col = Collision(bvalue.position.x, bvalue.position.y,  bvalue.r,  bvalue.r, tower.position.x, tower.position.y, tower.size, tower.size);
-  
+
+      col = Collision(bullet.position.x, bullet.position.y,  bullet.r,  bullet.r, tower.position.x, tower.position.y, tower.size, tower.size);
+
       if(col) {
         tower.health -= 10;
       }
 
-    }
-
-    // draw bullet only if within bounds, otherwise remove bullet
-    if (bvalue.position.x >= 0 && bvalue.position.x <= width && bvalue.position.y >= 0 && bvalue.position.y <= height && !col) {
-      bvalue.createParticle()
-    } else { 
-      bullets.splice(index, 1);
-      // console.log(bullets.length)
-    }
-
-    pop()
+    };
   });
 
+  // player 1
+  player.display();
+
+  // player 2
+  player_2.display();
+
   // abilities bar
-  push()
-  strokeWeight(2)
-  stroke(0, 0, 0);
-  fill(0, 0, 15);
-  rect(width/2 - 50, height - 51, 50, 50);
-  rect(width/2, height - 51, 50, 50);
-
-  push()
-  textSize(10);
-  fill(250);
-  text('Q', width/2 - 50, height - 40);
-  text('W', width/2 + 2, height - 40);
-  pop()
-
-  translate(width/2 - 42, height - 42);
-  drawHeart(35,35)
-  translate(50, 0);
-  
-  drawStar(35,35)
-
-  describe('two gold rectangles');
-  pop()
+  abilities_bar.display();
 
   // key functions
+  controls();
 
+}
+
+function controls(){
+  
   if (keyIsDown(LEFT_ARROW)) {
     player.position.x -= 5;
   }
@@ -231,14 +127,14 @@ function draw() {
     player.position.y += 5;
   }
 
-  // W
-  // if (keyIsDown(87)) {
-  //   // player_2.position.y -= 5;
-  // }
+  // Z
+  if (keyIsDown(90)) {
+    player_2.position.y += 5;
+  }
 
   // S
   if (keyIsDown(83)) {
-    player_2.position.y += 5;
+    player_2.position.y -= 5;
   }
 
   // D
@@ -329,6 +225,25 @@ function drawStar(w,h) {
   pop()
 }
 
+//
+function drawLine(){
+  push()
+  strokeWeight(4);
+  stroke(255, 204, 50);
+  let vec = createVector(mouseX - player.position.x, mouseY - player.position.y)
+
+  if(vec.mag()>=100){
+    // let normx = vec.x / vec.mag();
+    // let normy = vec.y / vec.mag();
+    // line(player.position.x, player.position.y, player.position.x + 100*normx, player.position.y + 100*normy);
+    line(player.position.x, player.position.y, mouseX, mouseY); // TEMP
+  }else{
+    line(player.position.x, player.position.y, mouseX, mouseY);
+  }
+  describe('a line running from player to mouse pos');
+  pop()
+
+}
 
 // load bullet on click
 function mouseClicked() {
@@ -356,6 +271,104 @@ class Unit {
     this.position = position.copy();
     this.size = size;
     this.health = 100;
+  }
+};
+
+class AbilityBar extends Unit {
+  constructor(position, size) {
+    super(position, size)
+  }
+
+  display() {
+    push()
+    strokeWeight(2)
+    stroke(0, 0, 0);
+    fill(0, 0, 15);
+
+    translate(this.position.x, this.position.y);
+    rect(0, 0, this.size, this.size);
+    rect(50, 0, this.size, this.size);
+
+    push()
+    textSize(10);
+    fill(250);
+    text('Q', 0, 12);
+    text('W', 52, 12);
+    pop()
+
+    translate(8, 9);
+    drawHeart(35,35)
+    translate(50, 0);
+    
+    drawStar(35,35)
+
+    describe('a square on the left of centre bottom screen with a heart and the letter Q, another square on the right of centre bottom screen with a sword.');
+    pop()
+  }
+
+}
+
+class Tower extends Unit {
+  constructor(position, size) {
+    super(position, size)
+  }
+
+  display() {
+    if (this.health > 0){
+
+      push();
+      strokeWeight(6.0);
+      
+      stroke(125+this.health, 100+this.health, 50);
+      fill(100);
+
+      translate(this.position.x, this.position.y);
+
+      rect(0, 0, this.size, this.size, 20, 15+(100/this.health), 10, 5+(1000/this.health));
+      describe('white rect with outline and round edges of different radii that changes upon declining health');
+      
+      // tower health bar
+      stroke(50, 50, 20);
+      rect(0, -20, BAR_SIZE, 5,1);
+
+      stroke(120, 60, 80);
+      rect(0, -20, (BAR_SIZE * this.health/100), 5,1);
+
+      pop();
+    }
+  }
+};
+
+class Player extends Unit {
+  constructor(position, size) {
+    super(position, size)
+  }
+
+  display() {
+
+    if (this.health>0) {
+      push()
+
+      // stroke(50,50,50)
+      fill(100);
+      // fill(50, 100);
+      noStroke();
+
+      translate(this.position.x, this.position.y);
+      circle(0, 0, this.size);
+      describe('white circle');
+      
+      // player health bar
+      strokeWeight(6.0);
+      stroke(50, 50, 20);
+
+      rect(-BAR_SIZE/2, -30, BAR_SIZE, 5,1);
+ 
+      stroke(120, 60, 80);
+      rect(-BAR_SIZE/2, -30, ( BAR_SIZE * this.health/100), 5,1);
+      pop();
+    }
+
   }
 };
 
@@ -392,6 +405,17 @@ class EnemyParticle {
     this.x+=this.xSpeed;
     this.y+=this.ySpeed;
   }
+
+  // get position
+  getPosition() {
+    return [this.x, this.y]
+  }
+
+  // get speed
+  getPosition() {
+    return [this.xSpeed, this.ySpeed]
+  }
+
 };
 
 // bullet particle class
@@ -409,15 +433,32 @@ class Particle {
 
   // particle creation
   createParticle() {
-    ellipse(this.position.x, this.position.y, this.r, this.r);
+    if (this.position.x >= 0 && this.position.x <= width && this.position.y >= 0 && this.position.y <= height){
+      
+      push();
+      // set colour
+      strokeWeight(1);
+      stroke(255, 204, 150);
+      fill(50, this.lifespan);
+
+      ellipse(this.position.x, this.position.y, this.r, this.r);
+      pop();
+
+    }else{
+      this.lifespan = 0;
+    };
   }
 
   // particle motion
   moveParticle() {
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.lifespan -= 2;
+    if (this.position.x >= 0 && this.position.x <= width && this.position.y >= 0 && this.position.y <= height){
+      this.position.x += this.direction.x * 1/10;
+      this.position.y += this.direction.y * 1/10;
+    }else{
+      this.lifespan = 0;
+    };
   }
+
   // check if particle is dead
   isDead(){
     return this.lifespan < 0;
